d34b04e9bf94f454d2e81cba66646f92d1fc92f1
Moyra_sc <noemisolac@gmail.com>
2022-07-26 18:53:07 +0000

    proyecto finalizado

--- src/js/component/taskList.jsx
+++ src/js/component/taskList.jsx
@@ -4,6 +4,7 @@ const TaskList = () =>{
 
     const [tasks, setTasks] = useState ("");
     const [taskOfList, setTaskOfList] = useState([]);
+    
 
     // Creo función manejadora de evento para que me guarde las tareas.
     const handleTask = (e) => {
@@ -29,8 +30,7 @@ const TaskList = () =>{
         <>
         <input
 				type="text"
-				// className="form-control col-6"
-                className="form-control mx-auto border text-dark fw-bolder"
+                className="form-control mx-auto border text-dark fw-bolder mb-4"
                 placeholder="Enter a task"
 				aria-label="Username"
                 onChange={(e)=>setTasks(e.target.value)}
@@ -38,11 +38,12 @@ const TaskList = () =>{
                 onKeyDown={handleTask}/>
         <ul>
             {taskOfList.length === 0 && <p className="p-4">No task. Add new task</p>}
-        {taskOfList.map((item, i) => {
+            {taskOfList.map((item, i) => {
             return (
-                <li key={i} className="d-flex justify-content-between text-dark p-2">
+                <li key={i} className="d-flex justify-content-between text-dark item">
+                    
                     {item}
-                    <p onClick={()=>deleteTask(i)}> X </p>
+                    <p className ="delete"onClick={()=>deleteTask(i)}> x </p>
                 </li>
             )
         })}
