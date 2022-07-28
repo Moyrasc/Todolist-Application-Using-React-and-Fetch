cb5fe4b46738d295bbdebed560b99d90295646c8
Moyra_sc <noemisolac@gmail.com>
2022-07-28 16:12:58 +0000

    arreglando la peticion delete

--- src/js/component/taskList.jsx
+++ src/js/component/taskList.jsx
@@ -1,87 +1,126 @@
 import React, { useEffect, useState } from "react";
 
 const TaskList = () => {
-
-    const [tasks, setTasks] = useState("");
-    const [taskOfList, setTaskOfList] = useState([]);
-    
-    useEffect(()=>{
-        const getTasks = async () =>{
-            try {
-                await fetch('https://assets.breatheco.de/apis/fake/todos/user/moyra')
-                .then(response =>response.json())
-                .then(data => setTaskOfList(data))
-            } catch (error) {
-                console.log(error)
-            }
-        }
-        getTasks()
-    },[])
-
-    // fetch call para sincronizar las tareas con el servidor cada vez que haya un cambio en la lista.
-    const putTasks = async () =>{
-        try {
-            fetch('https://assets.breatheco.de/apis/fake/todos/user/moyra', {
-      method: "PUT",
-      body: JSON.stringify(taskOfList),
-      headers: {
-        "Content-Type": "application/json"
+  const [tasks, setTasks] = useState("");
+  const [taskOfList, setTaskOfList] = useState([]);
+  const [loading, setLoading] = useState();
+ 
+  // petición a la API
+     useEffect(() => {
+    const getTasks = async () => {
+      setLoading(true);
+      try {
+        await fetch("https://assets.breatheco.de/apis/fake/todos/user/moyra")
+          .then((response) => response.json())
+          .then((data) => setTaskOfList(data));
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
       }
-    })
-        } catch (error) {
-            console.log(error)
-        }
+    };
+    getTasks();
+  }, []);
+
+  // fetch call para sincronizar las tareas con el servidor cada vez que haya un cambio en la lista.
+  const putTasks = async (taskOfList) => {
+    try {
+      await fetch("https://assets.breatheco.de/apis/fake/todos/user/moyra", {
+        method: "PUT",
+        body: JSON.stringify(taskOfList),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
-    
-
-
-    // Creo función manejadora de evento para que me guarde las tareas.
-    const handleTask = (e) => {
-        if (e.key === "Enter") {
-            if (tasks !== "") {
-                setTaskOfList([...taskOfList, {label:tasks}]);
-                setTasks("")
-            } else {
-                alert("Enter your task")
-            }
-            putTasks()
-        }
-        
+  };
+  // Llama a la función putTask solo cuando taskOflist no esté vacía
+  useEffect(() => {
+    if (taskOfList.length > 0) {
+      putTasks(taskOfList);
     }
-    //Creo función para eliminar las tareas
+  }, [taskOfList]);
 
-    const deleteTask = (indexToDelete) => {
-        setTaskOfList(
-            taskOfList.filter((task, taskIndex) => taskIndex !== indexToDelete)
-        )
+  //fetch call para eliminar todas las tareas
+  const deleteAllTask = async () => {
+    setTaskOfList([]);
+    try {
+      fetch("https://assets.breatheco.de/apis/fake/todos/user/moyra", {
+        method: "PUT",
+        body: JSON.stringify([taskOfList]),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <>
-            <div className="row flex-wrap justify-content-center align-items-center gap-2 mb-5 mx-2">
-                <div className="col-5 col-sm-3  p-0"></div>
-                <input
-                    type="text"
-                    className="form-control text-dark fw-bolder mb-4 w-75"
-                    placeholder="Enter a task"
-                    aria-label="Username"
-                    onChange={(e) => setTasks(e.target.value)}
-                    value={tasks}
-                    onKeyDown={handleTask} />
-            </div>
-            <ul>
-                {taskOfList.length === 0 && <p className=" d-flex justify-content-center p-0 ">No task. Add new task</p>}
-                {taskOfList.map((item, i) => {
-                    return (
-                        <li key={i} className="d-flex justify-content-center text-dark item d-block">
+  // Creo función manejadora de evento para que me guarde las tareas.
+  const handleTask = (e) => {
+    if (e.key === "Enter") {
+      if (tasks !== "") {
+        setTaskOfList([...taskOfList, { label: tasks, done: false }]);
+        setTasks("");
+      } else {
+        alert("Enter your task");
+      }
+    }
+  };
+  //Creo función para eliminar las tareas
 
-                            {item.label}
-                            <p className="delete m-0 " onClick={() => deleteTask(i)}> x </p>
-                        </li>
-                    )
-                })}
-            </ul>
-        </>
-    )
-}
+  const deleteTask = (indexToDelete) => {
+    setTaskOfList(
+      taskOfList.filter((task, taskIndex) => taskIndex !== indexToDelete)
+    );
+  };
+
+  return (
+    <>
+      <div className="row flex-wrap justify-content-center align-items-center gap-2 mb-5 mx-2">
+        <div className="col-5 col-sm-3 ">
+          <input
+            type="text"
+            className="form-control text-dark fw-bolder  mb-4"
+            placeholder="Enter a task"
+            aria-label="Username"
+            onChange={(e) => setTasks(e.target.value)}
+            value={tasks}
+            onKeyDown={handleTask}
+          />
+        </div>
+
+        <ul className="">
+          {taskOfList.length === 0 && (
+            <p className=" d-flex justify-content-center p-0 ">
+              No task. Add new task
+            </p>
+          )}
+          {taskOfList.map((item, i) => {
+            return (
+              <li
+                key={i}
+                className="d-flex justify-content-center text-center text-dark item fw-bold"
+              >
+                {item.label}
+                <p className="delete  text-end" onClick={() => deleteTask(i)}>
+                  {" "}
+                  x{" "}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-danger" onClick={deleteAllTask}>
+            Remove all task
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
 export default TaskList;
